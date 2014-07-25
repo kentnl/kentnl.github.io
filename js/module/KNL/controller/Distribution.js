be1d55db6b47618fd06f836d999e0a843341fcc7
Kent Fredric <kentfredric@gmail.com>
2014-07-26 10:40:16 +1200

    Make angular find the module instead of assuming its global

--- js/module/KNL/controller/Distribution.js
+++ js/module/KNL/controller/Distribution.js
@@ -1,4 +1,4 @@
-KNL.controller('Distribution',
+angular.module('KNL').controller('Distribution',
     [ '$scope', '$http', function($scope,$http) {
       $http.get('data/distributions.json').success(function(data){
         $scope.distributions = data;
