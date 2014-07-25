30d51ce9544d1eb8a83ed7b9a0ac743b49e415ce
Kent Fredric <kentfredric@gmail.com>
2014-07-26 08:52:35 +1200

    Fetch data via AJAX

--- js/module/KNL/controller/Distribution.js
+++ js/module/KNL/controller/Distribution.js
@@ -1,5 +1,7 @@
-KNL.controller('Distribution', function($scope) {
-  $scope.distributions = [
-    'MooX-Lsub'
-  ];
-});
+KNL.controller('Distribution',
+    [ '$scope', '$http', function($scope,$http) {
+      $http.get('data/distributions.json').success(function(data){
+        $scope.distributions = data;
+      });
+    }]
+);
