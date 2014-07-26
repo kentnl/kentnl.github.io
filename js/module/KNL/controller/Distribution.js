e03e1f849e5a417950e90795f01fdd12893e6f61
Kent Fredric <kentfredric@gmail.com>
2014-07-26 16:46:51 +1200

    Automatically fetch updated metadata every 60 seconds

--- js/module/KNL/controller/Distribution.js
+++ js/module/KNL/controller/Distribution.js
@@ -1,10 +1,14 @@
 angular.module('KNL').controller('Distribution',
-    [ '$scope', '$http','$routeParams', function($scope,$http,$routeParams) {
-      $http.get('data/distributions.json').success(function(data){
-        $scope.distributions = data;
-        if ( $routeParams['item'] ) {
-          $scope.item = $routeParams.item
-        }
-      });
+    [ '$scope', '$http','$routeParams','$interval', function($scope,$http,$routeParams,$interval) {
+      var update = function() {
+        $http.get('data/distributions.json').success(function(data){
+          $scope.distributions = data;
+          if ( $routeParams['item'] ) {
+            $scope.item = $routeParams.item
+          }
+        });
+      };
+      update();
+      $interval(update, 60000);
     }]
 );
