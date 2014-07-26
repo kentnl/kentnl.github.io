873101e7fc1bd6c19f127dd5e12afde8f74d3518
Kent Fredric <kentfredric@gmail.com>
2014-07-26 17:59:22 +1200

    Don't create new updaters every view switch, disable updater on single item view

--- js/module/KNL/controller/Distribution.js
+++ js/module/KNL/controller/Distribution.js
@@ -1,6 +1,8 @@
 angular.module('KNL').controller('Distribution',
-    [ '$scope', '$http','$routeParams','$interval', function($scope,$http,$routeParams,$interval) {
-      var update = function() {
+[       '$scope', '$http','$routeParams','$interval','$log','$rootScope',
+function($scope,   $http,  $routeParams,  $interval,  $log,  $rootScope) {
+
+      $scope.updater = function() {
         $http.get('data/distributions.json').success(function(data){
           $scope.distributions = data;
           if ( $routeParams['item'] ) {
@@ -8,7 +10,12 @@ angular.module('KNL').controller('Distribution',
           }
         });
       };
-      update();
-      $interval(update, 60000);
-    }]
-);
+
+      if ( $rootScope['updater_interval'] ) {
+        $interval.cancel( $rootScope.updater_interval );
+      }
+      if ( !$routeParams['item'] ) {
+        $rootScope['updater_interval'] = $interval( $scope.updater, 60000 );
+      }
+      $scope.updater();
+}]);
