114b7a9fcdfb6fcda663f24a02a526e82d5903f2
Kent Fredric <kentfredric@gmail.com>
2014-07-26 15:33:55 +1200

    Add multiple views

--- js/module/KNL.js
+++ js/module/KNL.js
@@ -1,1 +1,13 @@
-angular.module('KNL', [ ]);
+angular.module('KNL', ['ngRoute'], ['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/simple', {
+    templateUrl: 'partials/view/simple.html' ,
+    controller: 'Distribution'
+  });
+  $routeProvider.when('/detailed', {
+    templateUrl: 'partials/view/detailed.html' ,
+    controller: 'Distribution'
+  });
+  $routeProvider.otherwise({
+    redirectTo: '/simple'
+  });
+}]);
