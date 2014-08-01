4e03bebd7209f90ab95308466d15b60b5b4a8c91
Kent Fredric <kentfredric@gmail.com>
2014-08-01 13:58:03 +1200

    Go back to using serialized data source because gh API has a stupid ratelimit and no obvious way to let people use it without authentication

--- js/module/KNL/controller/Distribution.js
+++ js/module/KNL/controller/Distribution.js
@@ -2,14 +2,26 @@ angular.module('KNL').controller('Distribution',
 [       '$scope', '$http','$routeParams','$interval','$log','$rootScope',
 function($scope,   $http,  $routeParams,  $interval,  $log,  $rootScope) {
 
+      var providers = {
+        'api': {
+          source: 'https://api.github.com/orgs/kentnl/repos',
+          sorter: function(a,b)  { return a['pushed_at'].localeCompare(b['pushed_at']); },
+          mapper: function(item) { return item['name']; }
+        },
+        'data': {
+          source: 'data/distributions.json',
+          sorter: function(a,b) { return a.localeCompare(b); },
+          mapper: function(item) { return item; }
+        }
+      };
+
+      var provider = 'data';
+
+
       $scope.updater = function() {
-        $http.get('https://api.github.com/orgs/kentnl/repos', {cache: true}).success(function(data){
-          data.sort(function(a,b) {
-            a['pushed_at'].localeCompare(b['pushed_at']);
-          });
-          $scope.distributions = data.map(function(item){
-            return item['name'];
-          });
+        $http.get( providers[provider].source, {cache: true}).success(function(data){
+          data.sort( providers[provider].sorter );
+          $scope.distributions = data.map( providers[provider].mapper);
         });
       };
 
