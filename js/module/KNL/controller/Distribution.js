7de13f80159188c140e1ea3cb105fa0a5280a75f
Kent Fredric <kentfredric@gmail.com>
2014-08-01 14:15:44 +1200

    Document existing code

--- js/module/KNL/controller/Distribution.js
+++ js/module/KNL/controller/Distribution.js
@@ -2,6 +2,10 @@ angular.module('KNL').controller('Distribution',
 [       '$scope', '$http','$routeParams','$interval','$log','$rootScope',
 function($scope,   $http,  $routeParams,  $interval,  $log,  $rootScope) {
 
+      // Unfortunately, the native github API sucks for public access web pages
+      // and this endpoint gives more data than we need, ordered badly,'
+      // and requiring us to do stupid paginating shit, and incur stupid
+      // rate-limit penalties for the privilege.
       var providers = {
         'api': {
           source: 'https://api.github.com/orgs/kentnl/repos',
@@ -17,7 +21,8 @@ function($scope,   $http,  $routeParams,  $interval,  $log,  $rootScope) {
 
       var provider = 'data';
 
-
+      // This is the function that fetches JSON data, processes it
+      // and injects it into the page.
       $scope.updater = function() {
         $http.get( providers[provider].source, {cache: true}).success(function(data){
           data.sort( providers[provider].sorter );
@@ -25,13 +30,25 @@ function($scope,   $http,  $routeParams,  $interval,  $log,  $rootScope) {
         });
       };
 
+      // This controller may be invoked multiple times,
+      // but when it does, a previous instance of this controller will cease to be
+      // useful. So its important we cancel its updater so it doesn't needlessly pound
+      // web servers in the background without benefit.
+      //
+      // Or for that matter, create a forever growing collection of updaters
+      // that hammer the server, growing in number each time a user clicks a link >_>
       if ( $rootScope['updater_interval'] ) {
         $interval.cancel( $rootScope.updater_interval );
       }
+
+      // If we're in "single item" view, the name of the chosen item is in itself
+      // sufficient to generate the page without needing to query the data from github.
       if ( $routeParams['item'] ) {
         $scope.item = $routeParams.item
       }
 
+      // Otherwise, we're in full page mode, and we'll need data updated
+      // every 60 seconds, and a data-update hit will have to fire after loading the page.
       if ( !$routeParams['item'] ) {
         $rootScope['updater_interval'] = $interval( $scope.updater, 60000 );
         $scope.updater();
